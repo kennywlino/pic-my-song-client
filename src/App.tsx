452676695c5c8d75cfd0ca9c558a57d43ae9201b
Kenny W. Lino <kennywlino@gmail.com>
2023-03-02 18:06:30 -0800

    Defined FileUploadForm component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Hero from "./Hero";
 import Footer from "./Footer";
+import FileUploadForm from "./FileUploadForm";
 
 export default function App() {
   return (
-    <>
+    <div className="flex flex-col w-full min-h-screen overflow-x-hidden">
       <Hero />
+      <FileUploadForm />
       <Footer />
-    </>
+    </div>
   );
 };
---
+++ src/FileUploadForm.tsx
@@ -0,0 +1,7 @@
+export default function FileUploadForm() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <input type="file" className="file-input file-input-bordered file-input-lg file-input-primary" />
+    </div>
+  )
+}
