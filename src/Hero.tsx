821d7a175620ef7474576fe22d6fcae9e964364d
Kenny W. Lino <kennywlino@gmail.com>
2023-03-08 16:21:51 -0800

    Adding images and additional styling.

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,14 +1,20 @@
 export default function Hero() {
   return (
-  <div className="hero min-h-screen bg-base-200">
-    <div className="hero-content">
-      <div>
-        <h1 className="text-5xl font-bold">PicMySong</h1>
-        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <div className="flex gap-3">
-        <button className="btn btn-primary">Demo</button>
-        <button className="btn btn-accent">Try It Now!</button>
-      </div>
+  <div className="hero min-h-screen max-w-full bg-base-200">
+    <div className="hero-content flex-col">
+      <div className="flex justify-center items-center">
+        <div>
+          <h1 className="text-5xl font-bold">PicMySong</h1>
+          <p className="text-xl py-6">Are you looking for the perfect song for your picture? Just upload an image and get some suggestions based on what's in your picture.</p>
+          <div className="flex gap-3">
+            <button className="btn btn-primary">Demo</button>
+            <button className="btn btn-accent">Try It Now!</button>
+          </div>
+        </div>
+        <div className="flex">
+          <img src="https://img.icons8.com/clouds/400/null/image.png" className=""/>
+          <img src="https://img.icons8.com/clouds/400/null/audio-wave--v1.png" className=""/>
+        </div>
       </div>
     </div>
   </div>
--- src/Results.tsx
+++ src/Results.tsx
@@ -6,10 +6,20 @@
 export default function Results( props: { songData : object, image: string }) {
   const { songData, image } = props;
 
-  // const filteredSongData = filterSongData(props.songData);
-  console.log('image in results: ', image);
-  
+  let songData0;
 
+  if (Object.keys(songData).length !== 0) {
+
+    const allTracks = songData.data[0].tracks.items;
+    
+    songData0 = {
+      artist: allTracks[0].artists[0].name,
+      song: allTracks[0].name,
+      album: allTracks[0].album.name,
+      preview_url: allTracks[0].preview_url,
+      spotify_url: allTracks[0].external_urls.spotify
+    }
+  }
 
   return (
     <>
@@ -19,7 +29,7 @@ export default function Results( props: { songData : object, image: string }) {
           {image && <img src={image} alt="test" className="h-96"/> }
         </div>
         <div>
-        {Object.keys(songData).length !== 0 && JSON.stringify(songData, null, 4)}
+        {Object.keys(songData).length !== 0 && JSON.stringify(songData0, null, 4)}
         </div>
       </div>
     </>
