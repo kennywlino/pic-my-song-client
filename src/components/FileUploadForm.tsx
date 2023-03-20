0270310a63335ea1c8790e88908d122d8512e622
Kenny W. Lino <kennywlino@gmail.com>
2023-03-20 10:30:54 -0700

    Added a hook to call server on page load to try and fix Render sleep issue.

--- src/components/FileUploadForm.tsx
+++ src/components/FileUploadForm.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { type } from 'os';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { ReactComponent as Photo } from '../assets/photo-1263856.svg';
 
 const SERVER = import.meta.env.VITE_SERVER;
@@ -38,6 +38,15 @@ const fileHandler = (e: ChangeEvent<HTMLInputElement>, setSongData: Function, se
 }
 
 export default function FileUploadForm(props: { setSongData: Function, setImage: Function }) {
+  const [isServerRunning, setIsServerRunning] = useState(false);
+
+  useEffect(() => {
+    axios.get(`${SERVER}`)
+      .then(response => setIsServerRunning(true))
+      .catch(error => console.error(error));
+  }, []);
+
+
   return (
     <div className="min-h-screen flex justify-center items-center">
       <label htmlFor="image_upload" className="flex flex-col items-center justify-center h-96 w-4/5 px-4 border-2 border-dashed rounded-md border-gray-400 cursor-pointer bg-gray-700 hover:bg-gray-600">
@@ -45,6 +54,11 @@ export default function FileUploadForm(props: { setSongData: Function, setImage:
         <Photo className="h-20 w-20 fill-white" />
           <p className="my-2 text-xl"><span className="font-semibold">Click to upload</span> or drag and drop</p>
           <p className="text-lg">JPG or PNG files</p>
+          {isServerRunning ? 
+            <div className="alert alert-success mt-4">Server online</div> 
+            :
+            <div className="alert alert-error">Server offline</div> 
+          }
         </div>
         <input type="file" id="image_upload" className="hidden" onChange={(e) => fileHandler(e, props.setSongData, props.setImage)}/>
       </label>
